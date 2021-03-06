ce9d7afd55f027e9f7fb8512cb3ccf05700ad28c
kaye <tankaye@gmail.com>
2021-03-06 22:44:10 +0800

    login page fully functional

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,71 +1,13 @@
-// import { Form, Input, Button, Checkbox } from "antd";
-// import { UserOutlined, LockOutlined } from "@ant-design/icons";
-// import { Link } from "react-router-dom";
-
-// const Login = () => {
-//   const onFinish = (values) => {
-//     console.log("Received values of form: ", values);
-//   };
-
-//   return (
-//     <div class="login-register-form">
-//       <Form
-//         name="normal_login"
-//         className="login-register-form"
-//         initialValues={{ remember: true }}
-//         onFinish={onFinish}
-//       >
-//         <Form.Item
-//           name="email"
-//           rules={[{ required: true, message: "Please input your Email!" }]}
-//         >
-//           <Input
-//             prefix={<UserOutlined className="site-form-item-icon" />}
-//             placeholder="Email"
-//           />
-//         </Form.Item>
-//         <Form.Item
-//           name="password"
-//           rules={[{ required: true, message: "Please input your Password!" }]}
-//         >
-//           <Input
-//             prefix={<LockOutlined className="site-form-item-icon" />}
-//             type="password"
-//             placeholder="Password"
-//           />
-//         </Form.Item>
-//         <Form.Item>
-//           <Form.Item name="remember" valuePropName="checked" noStyle>
-//             <Checkbox>Remember me</Checkbox>
-//           </Form.Item>
-
-//           <a className="login-form-forgot" href="">
-//             Forgot password
-//           </a>
-//         </Form.Item>
-
-//         <Form.Item>
-//           <Button
-//             type="primary"
-//             htmlType="submit"
-//             className="login-register-form-button"
-//           >
-//             Log in
-//           </Button>
-//           Or <Link to="/Register">register now</Link>
-//         </Form.Item>
-//       </Form>
-//     </div>
-//   );
-// };
-
-// export default Login;
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
 import classnames from "classnames";
+
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
 class Login extends Component {
   constructor() {
     super();
@@ -96,85 +38,70 @@ class Login extends Component {
   onChange = (e) => {
     this.setState({ [e.target.id]: e.target.value });
   };
-  onSubmit = (e) => {
-    e.preventDefault();
+  onFinish = (values) => {
+    console.log(values);
     const userData = {
-      email: this.state.email,
-      password: this.state.password,
+      email: values.email,
+      password: values.password,
     };
     this.props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
   };
   render() {
     const { errors } = this.state;
     return (
-      <div className="container">
-        <div style={{ marginTop: "4rem" }} className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Login</b> below
-              </h4>
-              <p className="grey-text text-darken-1">
-                Don't have an account? <Link to="/register">Register</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.email || errors.emailnotfound,
-                  })}
-                />
-                <label htmlFor="email">Email</label>
-                <span className="red-text">
-                  {errors.email}
-                  {errors.emailnotfound}
-                </span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password || errors.passwordincorrect,
-                  })}
-                />
-                <label htmlFor="password">Password</label>
-                <span className="red-text">
-                  {errors.password}
-                  {errors.passwordincorrect}
-                </span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem",
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Login
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+      <Form
+        onFinish={this.onFinish}
+        name="normal_login"
+        className="login-register-form"
+      >
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: "Please input your Email!" }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Email"
+            onChange={this.onChange}
+            value={this.state.email}
+            error={errors.email}
+            id="email"
+            type="email"
+            className={classnames("", {
+              invalid: errors.email || errors.emailnotfound,
+            })}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            placeholder="Password"
+            onChange={this.onChange}
+            value={this.state.password}
+            error={errors.password}
+            id="password"
+            type="password"
+            className={classnames("", {
+              invalid: errors.password || errors.passwordincorrect,
+            })}
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-register-form-button"
+          >
+            Log in
+          </Button>
+          Or if you are a new staff member,{" "}
+          <Link to="/Register">register here!</Link>
+        </Form.Item>
+      </Form>
     );
   }
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,98 +1,11 @@
-// import { Form, Input, Button, Checkbox } from "antd";
-// import { MailOutlined, UserOutlined, LockOutlined } from "@ant-design/icons";
-// import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-
-// const Register = () => {
-//   const onFinish = (values) => {
-//     console.log("Received values of form: ", values);
-//   };
-
-//   return (
-//     <div class="login-register-form">
-//       <Form
-//         name="normal_register"
-//         className="login-register-form"
-//         initialValues={{ remember: true }}
-//         onFinish={onFinish}
-//       >
-//         <Form.Item
-//           name="name"
-//           rules={[{ required: true, message: "Please input your Name!" }]}
-//         >
-//           <Input
-//             prefix={<UserOutlined className="site-form-item-icon" />}
-//             placeholder="Name"
-//           />
-//         </Form.Item>
-//         <Form.Item
-//           name="email"
-//           rules={[{ required: true, message: "Please input your Email!" }]}
-//         >
-//           <Input
-//             prefix={<MailOutlined className="site-form-item-icon" />}
-//             placeholder="Email"
-//           />
-//         </Form.Item>
-//         <Form.Item
-//           name="password"
-//           rules={[{ required: true, message: "Please input your Password!" }]}
-//         >
-//           <Input
-//             prefix={<LockOutlined className="site-form-item-icon" />}
-//             type="password"
-//             placeholder="Password"
-//           />
-//         </Form.Item>
-//         <Form.Item
-//           name="password2"
-//           dependencies={["password"]}
-//           hasFeedback
-//           rules={[
-//             { required: true, message: "Passwords do not match!" },
-//             ({ getFieldValue }) => ({
-//               validator(_, value) {
-//                 if (!value || getFieldValue("password") === value) {
-//                   return Promise.resolve();
-//                 }
-
-//                 return Promise.reject(
-//                   new Error("The two passwords that you entered do not match!")
-//                 );
-//               },
-//             }),
-//           ]}
-//         >
-//           <Input
-//             prefix={<LockOutlined className="site-form-item-icon" />}
-//             type="password"
-//             placeholder="Confirm Password"
-//           />
-//         </Form.Item>
-
-//         <Form.Item>
-//           <Button
-//             type="primary"
-//             htmlType="submit"
-//             className="login-register-form-button"
-//           >
-//             Register
-//           </Button>
-//           Or <Link to="/Login">Login here</Link>
-//         </Form.Item>
-//       </Form>
-//     </div>
-//   );
-// };
-
-// export default Register;
-
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import classnames from "classnames";
+import { Form, Input, Button, Checkbox } from "antd";
+import { MailOutlined, UserOutlined, LockOutlined } from "@ant-design/icons";
 
 class Register extends Component {
   constructor() {
@@ -124,13 +37,12 @@ class Register extends Component {
   onChange = (e) => {
     this.setState({ [e.target.id]: e.target.value });
   };
-  onSubmit = (e) => {
-    e.preventDefault();
+  onFinish = (values) => {
     const newUser = {
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password,
-      password2: this.state.password2,
+      name: values.name,
+      email: values.email,
+      password: values.password,
+      password2: values.password2,
     };
     console.log(newUser);
     this.props.registerUser(newUser, this.props.history);
@@ -139,96 +51,109 @@ class Register extends Component {
   render() {
     const { errors } = this.state;
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Register</b> below
-              </h4>
-              <p className="grey-text text-darken-1">
-                Already have an account? <Link to="/login">Log in</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.name}
-                  error={errors.name}
-                  id="name"
-                  type="text"
-                  className={classnames("", {
-                    invalid: errors.name,
-                  })}
-                />
-                <label htmlFor="name">Name</label>
-                <span className="red-text">{errors.name}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.email,
-                  })}
-                />
-                <label htmlFor="email">Email</label>
-                <span className="red-text">{errors.email}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password,
-                  })}
-                />
-                <label htmlFor="password">Password</label>
-                <span className="red-text">{errors.password}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password2}
-                  error={errors.password2}
-                  id="password2"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password2,
-                  })}
-                />
-                <label htmlFor="password2">Confirm Password</label>
-                <span className="red-text">{errors.password2}</span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem",
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+      <Form
+        name="normal_login"
+        className="login-register-form"
+        onFinish={this.onFinish}
+      >
+        <Form.Item
+          name="name"
+          rules={[{ required: true, message: "Please input your Name!" }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Name"
+            onChange={this.onChange}
+            value={this.state.name}
+            error={errors.name}
+            id="name"
+            type="text"
+            className={classnames("", {
+              invalid: errors.name,
+            })}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: "Please input your Email!" }]}
+        >
+          <Input
+            prefix={<MailOutlined className="site-form-item-icon" />}
+            placeholder="Email"
+            onChange={this.onChange}
+            value={this.state.email}
+            error={errors.email}
+            id="email"
+            type="email"
+            className={classnames("", {
+              invalid: errors.email,
+            })}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            placeholder="Password"
+            onChange={this.onChange}
+            value={this.state.password}
+            error={errors.password}
+            id="password"
+            type="password"
+            className={classnames("", {
+              invalid: errors.password,
+            })}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="password2"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            { required: true, message: "Passwords do not match!" },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+
+                return Promise.reject(
+                  new Error("The two passwords that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            placeholder="Confirm Password"
+            onChange={this.onChange}
+            value={this.state.password2}
+            error={errors.password2}
+            id="password2"
+            type="password"
+            className={classnames("", {
+              invalid: errors.password2,
+            })}
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-register-form-button"
+          >
+            Register
+          </Button>
+          Or <Link to="/Login">Login here</Link>
+        </Form.Item>
+      </Form>
     );
   }
 }
