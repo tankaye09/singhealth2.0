9e4cf16ec7542b26aaae54b972ac58721ad34577
kaye <tankaye@gmail.com>
2021-03-06 19:03:24 +0800

    working login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,158 +1,190 @@
-import { Form, Input, Button, Checkbox } from "antd";
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
-import { Link } from "react-router-dom";
-
-const Login = () => {
-  const onFinish = (values) => {
-    console.log("Received values of form: ", values);
-  };
-
-  return (
-    <div class="login-register-form">
-      <Form
-        name="normal_login"
-        className="login-register-form"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="email"
-          rules={[{ required: true, message: "Please input your Email!" }]}
-        >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Email"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: "Please input your Password!" }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <a className="login-form-forgot" href="">
-            Forgot password
-          </a>
-        </Form.Item>
-
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-register-form-button"
-          >
-            Log in
-          </Button>
-          Or <Link to="/Register">register now</Link>
-        </Form.Item>
-      </Form>
-    </div>
-  );
-};
-
-export default Login;
-
 // import { Form, Input, Button, Checkbox } from "antd";
 // import { UserOutlined, LockOutlined } from "@ant-design/icons";
-// import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 
-// class Login extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       email: "",
-//       password: "",
-//       errors: {},
-//     };
-//   }
-
-//   onChange = (e) => {
-//     this.setState({ [e.target.id]: e.target.value });
-//     console.log(this.state.email);
+// const Login = () => {
+//   const onFinish = (values) => {
+//     console.log("Received values of form: ", values);
 //   };
 
-//   onFinish = (e) => {
-//     e.preventDefault();
-//     const userData = {
-//       email: this.state.email,
-//       password: this.state.password,
-//     };
-//     console.log(userData);
-//   };
-
-//   render() {
-//     const { errors } = this.state;
-//     return (
-//       <div class="login-register-form">
-//         <Form
-//           name="normal_login"
-//           className="login-register-form"
-//           initialValues={{ remember: true }}
-//           onFinish={this.onFinish}
+//   return (
+//     <div class="login-register-form">
+//       <Form
+//         name="normal_login"
+//         className="login-register-form"
+//         initialValues={{ remember: true }}
+//         onFinish={onFinish}
+//       >
+//         <Form.Item
+//           name="email"
+//           rules={[{ required: true, message: "Please input your Email!" }]}
 //         >
-//           <Form.Item
-//             name="email"
-//             onChange={this.onChange}
-//             value={this.state.email}
-//             error={errors.email}
-//             id="email"
-//             type="email"
-//             rules={[{ required: true, message: "Please input your email!" }]}
-//           >
-//             <Input
-//               prefix={<UserOutlined className="site-form-item-icon" />}
-//               placeholder="Email"
-//             />
-//           </Form.Item>
-//           <Form.Item
-//             name="password"
-//             onChange={this.onChange}
-//             value={this.state.password}
-//             error={errors.password}
-//             id="password"
+//           <Input
+//             prefix={<UserOutlined className="site-form-item-icon" />}
+//             placeholder="Email"
+//           />
+//         </Form.Item>
+//         <Form.Item
+//           name="password"
+//           rules={[{ required: true, message: "Please input your Password!" }]}
+//         >
+//           <Input
+//             prefix={<LockOutlined className="site-form-item-icon" />}
 //             type="password"
-//             rules={[{ required: true, message: "Please input your Password!" }]}
+//             placeholder="Password"
+//           />
+//         </Form.Item>
+//         <Form.Item>
+//           <Form.Item name="remember" valuePropName="checked" noStyle>
+//             <Checkbox>Remember me</Checkbox>
+//           </Form.Item>
+
+//           <a className="login-form-forgot" href="">
+//             Forgot password
+//           </a>
+//         </Form.Item>
+
+//         <Form.Item>
+//           <Button
+//             type="primary"
+//             htmlType="submit"
+//             className="login-register-form-button"
 //           >
-//             <Input
-//               prefix={<LockOutlined className="site-form-item-icon" />}
-//               type="password"
-//               placeholder="Password"
-//             />
-//           </Form.Item>
-//           <Form.Item>
-//             <Form.Item name="remember" valuePropName="checked" noStyle>
-//               <Checkbox>Remember me</Checkbox>
-//             </Form.Item>
-
-//             <a className="login-form-forgot" href="">
-//               Forgot password
-//             </a>
-//           </Form.Item>
-
-//           <Form.Item>
-//             <Button
-//               type="primary"
-//               htmlType="submit"
-//               className="login-register-form-button"
-//             >
-//               Log in
-//             </Button>
-//             Or <Link to="/Register">register now</Link>
-//           </Form.Item>
-//         </Form>
-//       </div>
-//     );
-//   }
-// }
+//             Log in
+//           </Button>
+//           Or <Link to="/Register">register now</Link>
+//         </Form.Item>
+//       </Form>
+//     </div>
+//   );
+// };
 
 // export default Login;
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+import classnames from "classnames";
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
+  }
+
+  componentDidMount() {
+    // If logged in and user navigates to Login page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+  // might be deprecated
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard"); // push user to dashboard when they login
+    }
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors,
+      });
+    }
+  }
+  onChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    this.props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
+  };
+  render() {
+    const { errors } = this.state;
+    return (
+      <div className="container">
+        <div style={{ marginTop: "4rem" }} className="row">
+          <div className="col s8 offset-s2">
+            <Link to="/" className="btn-flat waves-effect">
+              <i className="material-icons left">keyboard_backspace</i> Back to
+              home
+            </Link>
+            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+              <h4>
+                <b>Login</b> below
+              </h4>
+              <p className="grey-text text-darken-1">
+                Don't have an account? <Link to="/register">Register</Link>
+              </p>
+            </div>
+            <form noValidate onSubmit={this.onSubmit}>
+              <div className="input-field col s12">
+                <input
+                  onChange={this.onChange}
+                  value={this.state.email}
+                  error={errors.email}
+                  id="email"
+                  type="email"
+                  className={classnames("", {
+                    invalid: errors.email || errors.emailnotfound,
+                  })}
+                />
+                <label htmlFor="email">Email</label>
+                <span className="red-text">
+                  {errors.email}
+                  {errors.emailnotfound}
+                </span>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={this.onChange}
+                  value={this.state.password}
+                  error={errors.password}
+                  id="password"
+                  type="password"
+                  className={classnames("", {
+                    invalid: errors.password || errors.passwordincorrect,
+                  })}
+                />
+                <label htmlFor="password">Password</label>
+                <span className="red-text">
+                  {errors.password}
+                  {errors.passwordincorrect}
+                </span>
+              </div>
+              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                <button
+                  style={{
+                    width: "150px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem",
+                  }}
+                  type="submit"
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors,
+});
+export default connect(mapStateToProps, { loginUser })(Login);
