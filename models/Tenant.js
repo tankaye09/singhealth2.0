293a077d8211c5ae4b09f5e31bf1920750082092
kaye <tankaye@gmail.com>
2021-03-30 20:48:17 +0800

    added model tenant

---
+++ models/Tenant.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const TenantSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  institution: {
+    type: String,
+    required: true,
+  },
+  auditor: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("tenants", TenantSchema);
+module.exports = User;
