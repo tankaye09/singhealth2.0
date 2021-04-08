cb6acb61827d02877df2094075a7e42804b16571
kaye <tankaye@gmail.com>
2021-04-08 18:11:26 +0800

    link directory to checklist-

---
+++ client/src/reducers/tenantReducer.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { SET_SELECTED_TENANT } from "../actions/types";
+
+const initialState = {};
+
+// anonymous function
+export default function (state = initialState, action) {
+  console.log("in reducer");
+  switch (action.type) {
+    case SET_SELECTED_TENANT:
+      return action.payload;
+    default:
+      return state;
+  }
+}
