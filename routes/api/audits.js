76f8d783ddb6bfc6ea09a888b2c03cf7e97d45eb
kaye <tankaye@gmail.com>
2021-04-09 02:05:46 +0800

    commit

--- routes/api/audits.js
+++ routes/api/audits.js
@@ -5,32 +5,32 @@ let Audit = require("../../models/Audits");
 // @desc Register user
 // @access Public
 router.post("/add", (req, res) => {
-    console.log(req.body);
-    const newAudit = new Audit({
-        type: req.body.type,
-        catCounts: req.body.catCounts,
-        profcount: req.body.profcount,
-        gc_count: req.body.gc_count,
-        food_count: req.body.food_count,
-        health_count: req.body.health_count,
-        safety_count: req.body.safety_count,
-        total_score: req.body.total_score,
-        image: req.body.image,
-        date: req.body.date,
-        description: req.body.description,
-        location: req.body.location,
-    });
+  console.log(req.body);
+  const newAudit = new Audit({
+    type: req.body.type,
+    catCounts: req.body.catCounts,
+    profcount: req.body.profcount,
+    gc_count: req.body.gc_count,
+    food_count: req.body.food_count,
+    health_count: req.body.health_count,
+    safety_count: req.body.safety_count,
+    total_score: req.body.total_score,
+    image: req.body.image,
+    date: req.body.date,
+    description: req.body.description,
+    location: req.body.location,
+  });
 
-    newAudit
-        .save()
-        .then(() => res.json("Audit added!"))
-        .catch((err) => res.status(400).json("Error: " + err));
+  newAudit
+    .save()
+    .then(() => res.json("Audit added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 router.route("/").get((req, res) => {
-    Audit.find()
-        .then((data) => res.json(data))
-        .catch((err) => res.status(400).json("Error: " + err));
+  Audit.find()
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
